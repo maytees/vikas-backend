f266db0b3075e4a1580822850c6f9fbd166a2b04
maytees <mateespublicprofile@gmail.com>
2023-03-16 09:35:23 -0400

    working frontend

--- frontend/routes/[shortened].tsx
+++ frontend/routes/[shortened].tsx
@@ -1,5 +1,21 @@
-import { PageProps } from "$fresh/server.ts";
+import { Handlers, PageProps } from "$fresh/server.ts";
 
-export default function Greet(props: PageProps) {
-  return <div>Hello {props.params.shortened}</div>;
+interface Url {
+  link: string;
+}
+
+export const handler: Handlers<Url | null> = {
+  async GET(req: Request, ctx) {
+    const shortened = ctx.params.shortened;
+    const res = await fetch(`http://localhost:9000/link/${shortened}`);
+    const link = await res.text();
+    if (!link) return Response.redirect("/", 404);
+
+    // Redirect to link
+    return Response.redirect(link, 307);
+  },
+};
+
+export default function Page() {
+  return <div>asdf</div>;
 }
--- frontend/routes/index.tsx
+++ frontend/routes/index.tsx
@@ -1,23 +1,49 @@
 import { Head } from "$fresh/runtime.ts";
+import { Handlers, PageProps } from "$fresh/server.ts";
 import Counter from "../islands/Counter.tsx";
 
-export default function Home() {
+interface Url {
+  link: string;
+  shortened: string;
+}
+
+export const handler: Handlers<Url> = {
+  async GET(req: Request, ctx) {
+    const url = new URL(req.url);
+    const link = url.searchParams.get("link") || "";
+
+    // make post request to http://localhost:900/link with a json body of an object with "url" key, and the variable link as value
+    const res = await fetch("http://localhost:9000/link", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ url: link }),
+    });
+
+    const shortened = await res.text();
+    return ctx.render({ link, shortened });
+  },
+};
+
+export default function Home({ data }: PageProps<Url>) {
+  const { link, shortened } = data;
+
   return (
     <>
       <Head>
         <title>Vikas</title>
       </Head>
       <div class="p-4 mx-auto max-w-screen-md">
-        <img
-          src="/logo.svg"
-          class="w-32 h-32"
-          alt="the fresh logo: a sliced lemon dripping with juice"
-        />
-        <p class="my-6">
-          Welcome to `fresh`. Try updating this message in the
-          ./routes/index.tsx file, and refresh.
-        </p>
-        <Counter start={3} />
+        <form>
+          <input type="text" name="link" value={link} />
+          <button type="submit">Shorten</button>
+          {shortened && (
+            <div>
+              <a href={shortened}>{shortened}</a>
+            </div>
+          )}
+        </form>
       </div>
     </>
   );
